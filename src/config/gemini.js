e2f3f02119d0d1f018f1f1a72cac946e5ff1cdab
gokulzeus5 <gokulzeus837@gmail.com>
2025-09-20 20:16:22 +0530

    first commit step2

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,6 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-// Always pass API key explicitly in browser
+
+const VITE_GEMINI_API_KEY =  AIzaSyATNqpshAmJ9ZrzNXkuqGgEh7A_mKZ0jqE
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
